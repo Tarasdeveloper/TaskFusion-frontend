7eb4dc5eb6fe3e36fbcd3d08317166c703ad5803
Tarasdeveloper <tomahavki1@gmail.com>
2023-10-14 21:16:25 +0300

    commit

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -1,40 +1,57 @@
+import {
+  DescrImage,
+  DescrImgWrap,
+  DescrItem,
+  DescrItemMid,
+  DescrList,
+  DescrSpan,
+  DescrText,
+  DescrTitle,
+} from './Description.styled';
+
 const Description = () => {
   return (
-    <ul>
-      <li>
-        <span>1.</span>
+    <DescrList>
+      <DescrItem>
+        <DescrSpan>1.</DescrSpan>
         <p>Calendar</p>
-        <h2>VIEW</h2>
-        <p>
+        <DescrTitle>VIEW</DescrTitle>
+        <DescrText>
           GooseTrack&apos;s Calendar view provides a comprehensive overview of
           your schedule, displaying all your tasks, events, and appointments in
           a visually appealing and intuitive layout.
-        </p>
-        <img src="" alt="" />
-      </li>
-      <li>
-        <span>2.</span>
-        <h2>SIDEBAR</h2>
-        <p>
+        </DescrText>
+        <DescrImgWrap>
+          <DescrImage src="" alt="" />
+        </DescrImgWrap>
+      </DescrItem>
+      <DescrItemMid>
+        <DescrSpan>2.</DescrSpan>
+        <DescrTitle>SIDEBAR</DescrTitle>
+        <DescrText>
           GooseTrack offers easy access to your account settings, calendar, and
           filters. The &quot;My Account&quot; section allows you to manage your
           profile information and preferences, while the calendar provides a
           quick and convenient way to view your upcoming events and tasks.
-        </p>
-        <img src="" alt="" />
-      </li>
-      <li>
-        <span>3.</span>
+        </DescrText>
+        <DescrImgWrap>
+          <DescrImage src="" alt="" />
+        </DescrImgWrap>
+      </DescrItemMid>
+      <DescrItem>
+        <DescrSpan>3.</DescrSpan>
         <p>All in</p>
-        <h2>ONE</h2>
-        <p>
+        <DescrTitle>ONE</DescrTitle>
+        <DescrText>
           GooseTrack is an all-in-one productivity tool that helps you stay on
           top of your tasks, events, and deadlines. Say goodbye to scattered
           to-do lists and hello to streamlined productivity with GooseTrack.
-        </p>
-        <img src="" alt="" />
-      </li>
-    </ul>
+        </DescrText>
+        <DescrImgWrap>
+          <DescrImage src="" alt="" />
+        </DescrImgWrap>
+      </DescrItem>
+    </DescrList>
   );
 };
 
---
+++ src/components/Description/Description.styled.js
@@ -0,0 +1,39 @@
+import styled from 'styled-components';
+
+export const DescrList = styled.ul`
+  display: block;
+`;
+
+export const DescrItem = styled.li`
+  display: block;
+`;
+
+export const DescrItemMid = styled.li`
+  display: block;
+`;
+
+export const DescrSpan = styled.span`
+  color: #3e85f3;
+  font-family: Inter;
+  font-size: 104px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 1; /* 104px */
+  letter-spacing: -4px;
+`;
+
+export const DescrTitle = styled.h2`
+  display: block;
+`;
+
+export const DescrText = styled.p`
+  display: block;
+`;
+
+export const DescrImgWrap = styled.div`
+  display: block;
+`;
+
+export const DescrImage = styled.img`
+  display: block;
+`;
