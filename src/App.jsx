7dd8f79024887512578f00baf5e70cab3f964a9d
Roksolyanka <roksolyanakushnir@gmail.com>
2023-10-14 23:54:55 +0300

    AccountPage create

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,18 @@ import { AppWrapper } from './App.styled';
 import MainPage from './pages/MainPage/MainPage';
 import { RegisterPage } from './pages/RegisterPage/RegisterPage';
 import { LoginPage } from './pages/LoginPage/LoginPage';
+import AccountPage from './pages/AccountPage/AccountPage';
 import './fonts.css';
 
 function App() {
   return (
     <AppWrapper>
       <Routes>
-        <Route path="/" element={<MainPage />}>
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="*" element={<ErrorPage />} />
-        </Route>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/account" element={<AccountPage />} />
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
     </AppWrapper>
   );
