ca5163c31fb08480f1fb44c74292a14823f502b7
Burminskyi <119361840+Burminskyi@users.noreply.github.com>
2023-10-16 18:31:28 +0300

    Created auth slice, redux store, added redux-persist lib

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import { Provider } from 'react-redux';
+import { store } from './redux/store.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/TaskFusion-frontend">
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter basename="/TaskFusion-frontend">
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
 );
