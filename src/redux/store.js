b6fb1288c2e92d717205fa800c20536a3d965aa6
Oleksii <110523568+Oleksii-co@users.noreply.github.com>
2023-10-16 22:40:33 +0300

    add icon-btn theme toggler and connect with store

--- src/redux/store.js
+++ src/redux/store.js
@@ -12,15 +12,24 @@ import {
 } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
+import themeSlise from './theme/themeSlise';
+
 const authPersistConfig = {
   key: 'auth',
   storage,
   whitelist: ['token'],
 };
 
+const themePersistConfig = {
+  key: 'theme',
+  storage,
+};
+
 export const store = configureStore({
   reducer: {
     auth: persistReducer(authPersistConfig, authReducer),
+
+    theme: persistReducer(themePersistConfig, themeSlise),
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
---
+++ src/redux/theme/selectors.js
@@ -0,0 +1,1 @@
+export const themeSelect = (state) => state.theme.theme;
