38f354de1683bd7770440aeb7a680370f947d5e9
Burminskyi <119361840+Burminskyi@users.noreply.github.com>
2023-10-19 19:42:36 +0300

    added 404 page

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,10 +1,34 @@
-import { Container, Title } from './ErrorPage.styled';
+import { useNavigate } from 'react-router-dom';
+import {
+  Container,
+  ErrorImage,
+  ErrorImageItem,
+  ErrorPageBtn,
+  ErrorPageSecion,
+  ErrorText,
+} from './ErrorPage.styled';
+import image from './ErrorPage.svg';
 
 const ErrorPage = () => {
+  const navigate = useNavigate();
+  
+  function handleClick() {
+    navigate('/', { replace: true });
+  }
+
   return (
-    <Container>
-      <Title>404</Title>
-    </Container>
+    <ErrorPageSecion>
+      <Container>
+        <ErrorImage>
+          4<ErrorImageItem alt="" src={image} />4
+        </ErrorImage>
+        <ErrorText>
+          We’re sorry, the page you requested could not be found. Please go back
+          to the homepage.
+        </ErrorText>
+        <ErrorPageBtn onClick={handleClick}>Back to home</ErrorPageBtn>
+      </Container>
+    </ErrorPageSecion>
   );
 };
 
--- src/pages/ErrorPage/ErrorPage.styled.jsx
+++ src/pages/ErrorPage/ErrorPage.styled.jsx
@@ -1,18 +1,107 @@
 import styled from '@emotion/styled';
 
-export const Container = styled.div`
+export const ErrorPageSecion = styled.section`
+  height: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
-  padding-top: 500px;
 `;
 
-export const Title = styled.h1`
-  color: #ffffff;
+export const Container = styled.div`
+  width: 375px;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  padding-left: 48px;
+  padding-right: 48px;
+  @media (min-width: 768px) {
+    width: 768px;
+    padding-left: 132px;
+    padding-right: 132px;
+  }
+`;
+
+export const ErrorImage = styled.div`
+  width: 252px;
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  color: #3e85f3;
   text-shadow:
-    0 0 5px #00baff,
-    0 0 10px #00baff,
-    0 0 20px #00baff,
-    0 0 40px #00baff,
-    0 0 80px #00baff;
+    0px 9.399999618530273px 57.6875px rgba(0, 0, 0, 0.04),
+    0px 47px 355px rgba(0, 0, 0, 0.07);
+  font-family: Inter;
+  font-size: 100px;
+  font-weight: 700;
+  line-height: 150px; /* 150% */
+  @media (min-width: 768px) {
+    width: 503px;
+    font-size: 200px;
+    line-height: 250px; /* 125% */
+    margin-bottom: 40px;
+  }
+`;
+
+export const ErrorText = styled.p`
+  display: inline-block;
+  margin-bottom: 24px;
+  color: rgba(17, 17, 17, 0.7);
+
+  text-align: center;
+  font-family: Inter;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 18px; /* 128.571% */
+  @media (min-width: 768px) {
+    width: 387px;
+    margin-bottom: 32px;
+  }
+`;
+
+export const ErrorPageBtn = styled.button`
+  padding: 14px 32px;
+
+  border-radius: 16px;
+  border: none;
+  background: #3e85f3;
+  box-shadow: 4px 2px 16px 0px rgba(136, 165, 191, 0.48);
+
+  color: #fff;
+  font-family: Inter;
+  font-size: 14px;
+  font-weight: 600;
+  line-height: 18px; /* 128.571% */
+  letter-spacing: -0.28px;
+
+  transition:
+    background 0.3s,
+    color 0.3s;
+
+  &:hover {
+    background: #21222c;
+    color: #3e85f3;
+  }
+  &:focus {
+    background: #21222c;
+    color: #3e85f3;
+  }
+  @media (min-width: 768px) {
+    padding: 16px 48px;
+  }
+`;
+
+export const ErrorImageItem = styled.img`
+  display: inline-block;
+  position: absolute;
+  left: 42px;
+  @media (min-width: 768px) {
+    height: 435px;
+    left: 70px;
+  }
 `;
