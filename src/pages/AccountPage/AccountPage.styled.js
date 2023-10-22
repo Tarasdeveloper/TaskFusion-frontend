8a95a956d96039d70091bd50bedcbd6f25d163ce
Roksolyanka <roksolyanakushnir@gmail.com>
2023-10-22 23:00:19 +0300

    scroll done

--- src/pages/AccountPage/AccountPage.styled.js
+++ src/pages/AccountPage/AccountPage.styled.js
@@ -4,6 +4,4 @@ export const UserFormContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 100vh;
-  overflow-y: auto;
 `;
--- src/pages/MainLayout/MainLayout.styled.jsx
+++ src/pages/MainLayout/MainLayout.styled.jsx
@@ -1,12 +1,22 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-  overflow: hidden;
+  // overflow: hidden;
 `;
-export const WrapperForFixSideBar = styled.div``;
+export const WrapperForFixSideBar = styled.div`
+  // overflow: hidden;
+`;
 export const MainWrapper = styled.div`
   @media screen and (min-width: 1440px) {
     margin-left: 289px;
     padding: 0 32px;
+    // overflow: hidden;
+  }
+`;
+
+export const Main = styled.main`
+  @media screen and (min-width: 1440px) {
+    height: 80vh;
+    overflow-y: auto;
   }
 `;
