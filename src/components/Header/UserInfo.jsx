4e1677d81ccd7f79699883e4506cd6e1dedd9b6d
TsymbalyukM <mashatsymbaliuk2@gmail.com>
2023-10-23 18:57:31 +0300

    fix userinfo

--- src/components/Header/UserInfo.jsx
+++ src/components/Header/UserInfo.jsx
@@ -7,14 +7,14 @@ import sprite from '../../assets/sprite.svg';
 const UserInfo = () => {
   const { name, avatar } = useSelector(selectUser);
   const userName = name?.trim().split('');
-  const [userPhotoPreview, setUserPhotoPreview] = useState(false);
-  const handleImageError = () => {
-    setUserPhotoPreview(true);
-  };
+  // const [userPhotoPreview, setUserPhotoPreview] = useState(false);
+  // const handleImageError = () => {
+  //   setUserPhotoPreview(true);
+  // };
   return (
     <Wrapper>
       <UserName>{userName}</UserName>
-      {userPhotoPreview ? (
+      {avatar ? (
         <Avatar>
           <img src={avatar} alt="Avatar" />
         </Avatar>
